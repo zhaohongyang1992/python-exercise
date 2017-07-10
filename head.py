e33ecbed0ea5938eedc7140b3fc7a91136ad9d3f
zhaohongyang <hongyang_zhao@126.com>
2017-07-10 18:49:32 +0800

    add jingdong head.py

---
+++ head.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+import sys
+
+try:
+    output_num = int(sys.argv[1])
+except IOError as msg:
+    output_num = 5
+
+lines = sys.stdin.read().strip()
+line_list = lines.split('\n')
+
+for line in line_list[0:output_num]:
+    print(line)
